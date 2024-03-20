6b21f491c5abc0ae5a8e2470a2cec11ba6305f69
Mickael Zana <zanamickael@gmail.com>
2024-03-20 23:29:11 +0200

    Authentication flow redirection

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 import { Box, ThemeProvider, createTheme } from '@mui/material';
-import { Register } from './pages/Register';
-import { Login } from './pages/Login';
+import { useAuthContext } from './auth/useAuthContext';
+import Register from './pages/Register';
 import { Dashboard } from './pages/Dashboard';
 
 
-
 const App = () => {
+  const { isAuthenticated } = useAuthContext() || {};
 
   const theme = createTheme({
     palette: {
@@ -20,9 +21,9 @@ const App = () => {
       <Router>
         <Box sx={{ backgroundColor: 'black', height:'100vh'}}>
           <Routes>
-            <Route path="/register" element={<Register/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/dashboard" element={<Dashboard/>} />
+            <Route path="/register" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Register />} />
+            <Route path="/login" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login />} />
+            <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} />
             <Route path="*" element={<Navigate to="/login" />} />
           </Routes>
         </Box>
