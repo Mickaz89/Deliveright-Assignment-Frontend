46229b47c3c91cf8d369006576d9aab498d369db
Mickael Zana <zanamickael@gmail.com>
2024-03-20 23:56:12 +0200

    Replace mui/styles by mui/system - Dashboard page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import { TextField, useTheme, TextFieldProps } from '@mui/material';
-import { makeStyles, createStyles } from '@mui/styles';
+import { TextField, TextFieldProps } from '@mui/material';
+import { styled } from '@mui/system';
 
-import { Theme } from '@mui/material';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      '& .MuiOutlinedInput-root': {
-        '& fieldset': {
-          borderColor: theme.palette.grey[700],
-        },
-        '&:hover fieldset': {
-          borderColor: theme.palette.primary.dark,
-        },
-        '&.Mui-focused fieldset': {
-          borderColor: theme.palette.primary.dark,
-        },
-      },
-      '& .MuiInputLabel-root': {
-        color: theme.palette.primary.light,
-      },
-      '& .MuiInputBase-input': {
-        color: theme.palette.grey[400],
-        '&::placeholder': {
-            color: theme.palette.grey[300],
-          },
-      },
+const StyledInput = styled(TextField)(({ theme }) => ({
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: theme.palette.grey[700],
     },
-  })
-);
+    '&:hover fieldset': {
+      borderColor: theme.palette.primary.dark,
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: theme.palette.primary.dark,
+    },
+  },
+  '& .MuiInputLabel-root': {
+    color: theme.palette.primary.light,
+  },
+  '& .MuiInputBase-input': {
+    color: theme.palette.grey[400],
+    '&::placeholder': {
+      color: theme.palette.grey[300],
+    },
+  },
+}));
 
 
 interface InputProps {
@@ -44,16 +38,12 @@ interface InputProps {
 
 export const Input: React.FC<InputProps> = ({ label, sx, onChange, value, placeholder, inputProps, fullWidth }) => {
 
-    const classes = useStyles();
-    const theme: Theme = useTheme();
-
     return (
-        <TextField
+        <StyledInput
             fullWidth={fullWidth}
             onChange={onChange}
             value={value}
             sx={sx}
-            className={classes.root}
             label={label}
             placeholder={placeholder}
             InputProps={inputProps}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,61 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Box, Grid, IconButton, InputAdornment, Paper, Theme, useTheme } from '@mui/material';
+import { AddCircleOutlineOutlined, ArrowCircleUpOutlined, Visibility } from '@mui/icons-material';
+import { useAuthContext } from '../auth/useAuthContext';
+import CustomButton from '../components/CustomButton';
+import { Input } from '../components/Input';
 
 export const Dashboard = () => {
+    const theme: Theme = useTheme();
+    const { logout } = useAuthContext();
+
+    const [input, setInput] = React.useState<string>('');
+    const [disabled, setDisabled] = React.useState<boolean>(true);
+
+    React.useEffect(() => {
+        setDisabled(input === '');
+    }, [input]);
+
     return (
-        <Box>
-            Dashboard
+        <Box height="100%">
+            <Box p={5}>
+                <CustomButton onClick={() => logout()}>
+                    Logout
+                </CustomButton>
+            </Box>
+                <Grid container justifyContent="center">
+                    <Grid item xs={10} sm={10} md={8} lg={6}>
+                        <Input
+                            fullWidth
+                            onChange={(e) => setInput(e.target.value)}
+                            value={input}
+                            placeholder='Add task'
+                            inputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <AddCircleOutlineOutlined sx={{ color: theme.palette.grey[700] }} />
+                                    </InputAdornment>
+                                ),
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton disabled={disabled}>
+                                            <ArrowCircleUpOutlined sx={{ color: disabled ? theme.palette.grey[700] : theme.palette.primary.dark }} />
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <Box mt={10}>
+                            <Paper elevation={3 }>
+                                <Box p={5}>
+                                    Hello
+                                </Box>
+                            </Paper>
+                        </Box>
+                    </Grid>
+                </Grid>
         </Box>
-    )
-}
+    );
+};
+
+export default Dashboard;
